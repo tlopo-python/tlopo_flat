fc43afc6f4099691a7a5a1a0190beebc427cb171
tlopo <tlopo@github.com>
2025-10-04 16:54:26 +0400

    feat: Use argparse to allow custom separator

--- tlopo_flat/__main__.py
+++ tlopo_flat/__main__.py
@@ -1,7 +1,15 @@
 import yaml
 import sys
 from tlopo_flat import Flat
+import argparse
+
+parser = argparse.ArgumentParser(description="Flattens JSON and YAML structures")
+parser.prog = 'python -mtlopo_flat'
+parser.add_argument("-s", '--separator', help="Key separator, default= ' | '", default=" | ")
+args = parser.parse_args()
+
+separator = args.separator
+
 
 d = yaml.safe_load(sys.stdin.read())
-
-print(Flat(separator=".").to_string(d))
+print(Flat(separator=separator).to_string(d))
